f9e3777ccff9fcc384670d8512517fbd3d45cdcb
Lisa Herzberg <lisamarieherzberg@gmail.com>
2024-11-02 22:07:28 -0600

    last update tonight

--- app/HomePage/Summary.tsx
+++ app/HomePage/Summary.tsx
@@ -5,8 +5,8 @@ const Summary: React.FC = () => {
     <div className="flex items-start justify-between mx-30 my-20 py-10">
       <div className="text-left max-w-2xl">
         <p className="text-3xl font-openSans font-bold text-gray-700 ">
-          I am a dedicated full-stack developer specializing in React and React Native, 
-          with a strong foundation in TypeScript, and Node.js. 
+        I am a dedicated full-stack developer specializing in React, React Native, and Node.js, 
+        with a strong foundation in TypeScript. 
         </p>
       </div>
     </div>
